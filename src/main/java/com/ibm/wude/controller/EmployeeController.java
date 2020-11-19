e08037da4ae99acba75acf52f31d4314fa2b6390
vae2020 <2103263165@qq.com>
2020-11-19 16:57:38 +0800

    ver0.1.5

--- src/main/java/com/ibm/wude/controller/EmployeeController.java
+++ src/main/java/com/ibm/wude/controller/EmployeeController.java
@@ -3,13 +3,16 @@ package com.ibm.wude.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ibm.wude.model.EmployeeModel;
 import com.ibm.wude.service.EmployeeService;
 
+@CrossOrigin
 @RestController
 public class EmployeeController {
 
@@ -22,7 +25,8 @@ public class EmployeeController {
 		return list;
 	}
 	
-	@GetMapping("/addEmploy")
+	//@GetMapping("/addEmploy")
+	@PostMapping("/addEmploy")
 	public int addEmploy(@RequestBody EmployeeModel employee) {
 		return employeeService.addEmploy(employee);
 	}
